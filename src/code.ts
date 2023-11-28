5096989a7f81c447e84bd6cfcd9b68857b51990f
rdaud <rodrigodaudskeff@gmail.com>
2023-11-28 13:27:41 -0300

    Fixed Remove Links Bug

--- src/code.ts
+++ src/code.ts
@@ -1,63 +1,100 @@
 
 figma.showUI(__html__,{ themeColors: true});
-figma.ui.resize(350,200);
+figma.ui.resize(280,260);
 
+interface NotificationOptions {
+  timeout?: number;
+  error?: boolean;
+}
+
+function checkNodesSelection() {
+
+  const amountOfNodesSelected = figma.currentPage.selection.length
+
+  // Check if any node is selected at all
+  if (amountOfNodesSelected > 0) {
+    // Check if node type is COMPONENT or COMPONENT_SET
+    const array = figma.currentPage.selection
+    const checkNodeType = (node) =>  node.type === 'COMPONENT' || node.type === 'COMPONENT_SET';
+    const checkNodeTypeResult = array.every(checkNodeType)
+
+    if (checkNodeTypeResult) {
+      // If selection is allowed post a message
+      figma.ui.postMessage({
+        state: "SELECTION_ALLOWED",
+        amount: array.length
+      });
+    } else {
+      // If selection isn't allowed post a message
+      figma.ui.postMessage({
+        state: "SELECTION_NOT_ALLOWED"
+      });
+    }
+
+    } else {
+      // If nothing is selected, post a message
+      figma.ui.postMessage({
+        state: "NOTHING_SELECTED"
+      });
+    }
+}
+
+figma.on('run', () => {
+
+  checkNodesSelection()
+ 
+ })
 
 figma.on('selectionchange', () => {
 
-  if (figma.currentPage.selection.length > 0) {
-    for (const node of figma.currentPage.selection) {
-      if (node.type === 'COMPONENT') {
-        figma.ui.postMessage('Component selected');
-      } else {
-        figma.ui.postMessage('Please only select components');
-      }
-    }
-  } else {
-    figma.ui.postMessage('Nothing selected');
-  }
- 
-  
+ checkNodesSelection()
+
 })
 
+
+
+
 const showNotification = () => {
-  
+
   const amountOfNodesSelected = figma.currentPage.selection.length
 
+
   if (amountOfNodesSelected > 1) {
-    figma.notify(`${amountOfNodesSelected} components updated.`, {
-      timeout: 1000
+    figma.notify(`${amountOfNodesSelected} components updated!`, {
+      timeout: 2000
     })
   } else {
-    figma.notify(`${amountOfNodesSelected} component updated.`, {
-      timeout: 1000
+    figma.notify(`${amountOfNodesSelected} component updated!`, {
+      timeout: 2000
     })
   }
 }
 
 function updateDocumentation(data) {
 
- 
-      for (const node of figma.currentPage.selection) {
+    const { url } = data
+    const array = figma.currentPage.selection
 
-        if (node.type !== 'COMPONENT') {
-          figma.notify("You must select a component.", { error: true })
-          return
-        }
+    // Check if node type is COMPONENT or COMPONENT_SET
+    const checkNodeType = (node) =>  node.type === 'COMPONENT' || node.type === 'COMPONENT_SET';
+    const checkNodeTypeResult = array.every(checkNodeType)
+    
+    if(!checkNodeTypeResult) {
+      figma.notify("You must select only main components or components set!", { error: true })
+      figma.ui.postMessage('Update failed');
+      return
+    }
 
-        if (!data) {
-          node.documentationLinks = []
+    // Iterate in the current page selection
+    for (const node of figma.currentPage.selection) { 
+      if (node.type === 'COMPONENT_SET' || node.type === 'COMPONENT') {
+          node.documentationLinks = !!url && [{uri: url}] || []
+          figma.ui.postMessage('Update success');
           showNotification()
-        }
-
-        node.documentationLinks = [{uri: data}]
-        showNotification()
-
       }
+    }
      
-  }
-
-  
+}
 
 figma.ui.onmessage = msg => {
 
--- src/ui.tsx
+++ src/ui.tsx
@@ -1,106 +1,166 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { MantineProvider } from '@mantine/core';
-import { Flex, Container, Button, Text, TextInput, Space } from "@mantine/core";
+import { Flex, Container, Button, Text, TextInput, Space, Group, Stack } from "@mantine/core";
+import { z } from 'zod';
+import { useForm, zodResolver } from '@mantine/form';
 
 
+const descriptionText = "The link should start with \"https://\" or \"http://\""
+
+const schema = z.object({
+    url: z.string().url({ message: descriptionText })
+    .optional()
+    .or(z.literal('')),
+});
+
+interface FormValues {
+    url: string | undefined; // regular field, same as inferred type
+  }
 
 function App() {
+  
+  const form = useForm({
+    initialValues: {
+      url: ''
+    },
+    validateInputOnChange: true,
+    validate: zodResolver(schema),
+    clearInputErrorOnChange: true 
+  });
 
-    const [ buttonState, setButtonState ] = React.useState(false);
-    const [ value, setValue ] = React.useState('');
-
-    const onApply = () => {
-        parent.postMessage({ pluginMessage: { 
-            type: "apply",
-            data: value
-         } }, "*");
-
-         // Clear input
-         setValue('')
-    };
-
-    function checkURL() {
-        const pattern = /(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?/g;
-        console.log(pattern.test(value))
-
-    }
-
-  React.useEffect(() => {
-    checkURL()
-  },[value])
-
-    onmessage = ({ data }) => {
-        if (data.pluginMessage === 'Component selected') {
-            setButtonState(true)
-        } else if (data.pluginMessage === 'Component not selected') {
-            setButtonState(false)
-        } else {
-            setButtonState(false)
-        }
-    }
+  const [ loading, setLoading ] = React.useState(false)
+  const [ componentsAmount, setComponentsAmount ] = React.useState(0)
+  const [ isSelectionValid, setIsSelectionValid ] = React.useState(true)
 
   
+   // Get inferred form values type, will be `{ name: string | null }`
+   type FormValues = typeof form.values
+
+
+   // Use values type in handleSubmit function or anywhere else
+   const handleSubmit = (value: FormValues) => {
+    
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+            parent.postMessage({ pluginMessage: { 
+                type: "apply",
+                data: value
+            } }, "*");         
+            form.reset()
+        }, 1000)
+
+   };
+
+   onmessage = ({ data }) => {
+
+    const { state, amount } = data.pluginMessage
+
+    switch (state) {
+        case 'SELECTION_ALLOWED':
+          setComponentsAmount(amount)
+          setIsSelectionValid(true)
+          break;
+        case 'SELECTION_NOT_ALLOWED':
+            setComponentsAmount(0)
+            setIsSelectionValid(false)
+          break;
+        case 'NOTHING_SELECTED':
+            setComponentsAmount(0) 
+            setIsSelectionValid(true)
+            break;
+        default:
+          console.log(`No message received.`);
+        }
+   
+    }
+
     return (
         <MantineProvider withGlobalStyles withNormalizeCSS>
-            <Container
-                size="m"
-                px="24"
-                style={{
-                    "paddingTop": 24,
-                    "paddingBottom": 24,
-                    "height": 120
-                }}
+
+        <Container
+        size="m"
+        px="24"
+        style={{
+            "paddingTop": 24,
+            "width": "100%",
+        }}
+    >
+        <Flex
+            gap="md"
+            direction="column"
+            justify="center"
+            rowGap="md"
+            align="center"
+        >
+            <Stack
+                bg="var(--mantine-color-blue-light)"
+                justify="center"
+                h={50}
             >
-                <Flex
-                    gap="md"
-                    direction="column"
-                    justify="center"
-                    rowGap="md"
-                >
-                    <Text
-                        fz="sm"
-                    >
-                        Add the documentation link and click apply.
-                    </Text>
-                       
-                            <TextInput
-                                type="url"
-                                placeholder="https://amazingdesignsystem.com/components"
-                                value={value}
-                                onChange={(event) => setValue(event.currentTarget.value)}
-                                pattern="/(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?/g;
-                                "
-                                error={buttonState ? true : false}
-                            />
+
+        { isSelectionValid ?
+            <Text
+            size="xs"
+            >       
+            <b>{componentsAmount}</b> component(s) selected
+            </Text>
+             :
+             <Text size="xs" c="red" align="center">
+                You must only select main components or components set.
+            </Text>
+        }
+
+        </Stack>
+          
+            
+            <form onSubmit={form.onSubmit(handleSubmit)}>                       
+                    <TextInput
+                        placeholder="Link to documentation"
+                        {...form.getInputProps('url')}
+                        aria-label="Documentation link"
+                        description={ descriptionText }
+                        inputWrapperOrder={['label', 'input', 'description']}
+                        styles={{
+                            description: {
+                              color: !form.isValid() && "red"
+                            },
+                        }}
+                    />
+                    <Space h="md"/>
+                    <Button
+                        disabled={ form.isValid() && componentsAmount > 0 ? false : true }
+                        variant="filled"
+                        fullWidth={ true }
+                        className="brand"
+                        type="submit"
+                        loading={loading}
+                        radius="md"
+                        style={{
+                            "fontSize": "12px"
+                        }}
+                        >
                     
-                    <Space />
-                    <footer>
-                        {
-                            !!buttonState ?
-                            
-                            <Button
-                                variant="filled"
-                                onClick={onApply}
-                                fullWidth={true}
-                                className="brand"
-                            >
-                            Apply
-                            </Button> :
-                            <Button
-                                disabled
-                                variant="filled"
-                                onClick={onApply}
-                                fullWidth={true}
-                                className="brand"
-                            >
-                                Apply
-                            </Button>
-                        }
-                            
-                    </footer>
-                </Flex>
-            </Container>  
+                        Update Link
+                    </Button>
+                
+            
+            </form>            
+        </Flex>
+       
+        <Group justify="space-between" preventGrowOverflow={false} style={{"width": "100%"}}>
+            {/* <Text c="dimmed" size="xs" style={{"fontSize": "8px"}}> Created and maintained by <a href="https://www.linkedin.com/rdaud">Rodrigo Daud</a></Text> */}
+            <Text c="dimmed" size="xs" style={{"fontSize": "8px"}}> v1.0.0</Text>
+        </Group>
+       
+
+    </Container>
+            
+            
+    
+
+           
         </MantineProvider>
     );
   }
